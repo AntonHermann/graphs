a0870d66c20e6b66ee0d11d220aedafe94777314
AntonHermann <antonoehler@gmx.de>
2018-04-22 04:05:26 +0200

    added a test for Ord on Weight

--- src/graphs/tests/mod.rs
+++ src/graphs/tests/mod.rs
@@ -1,1 +1,11 @@
 pub mod graph_implementations;
+
+use graphs::*;
+
+#[test]
+fn test_weight_order() {
+    assert_eq!(Weight::Infinity, Weight::Infinity, "inf == inf");
+    assert!(Weight::Infinity > Weight::W(5), "inf > 5");
+    assert!(Weight::W(5) < Weight::Infinity, "5 < inf");
+    assert!(Weight::W(3) < Weight::W(5), "3 < 5");
+}
